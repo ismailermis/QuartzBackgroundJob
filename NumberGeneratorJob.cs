91ea867f583819a34faff1acb7f60380b3de0133
İsmail ERMİŞ <ismail.ermis@demirorenteknoloji.com>
2021-04-21 13:50:33 +0300

    initial Commit

---
+++ NumberGeneratorJob.cs
@@ -0,0 +1,21 @@
+﻿using Quartz;
+using System;
+using System.Threading.Tasks;
+
+namespace QuartzBackgroundJob
+{
+    public class NumberGeneratorJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            Console.WriteLine($"Your # is:  {RandomNumber(1000, 9999)}");
+            await Task.CompletedTask;
+        }
+
+        private int RandomNumber(int min, int max)
+        {
+            var random = new Random();
+            return random.Next(min, max);
+        }
+    }
+}
